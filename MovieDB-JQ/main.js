a4aec2c8cb4bec1afca5d4227e5ce10a82dace10
Yanina <yanina.pavlyk@gmail.com>
2019-01-09 22:59:41 +0200

    JQuery API calls

---
+++ MovieDB-JQ/main.js
@@ -0,0 +1,241 @@
+//--------------------------------------------- Storage Object -----------------------------
+var storage = {
+<<<<<<< HEAD
+    url: "https://api.themoviedb.org/3/",
+    imgUrl: "https://image.tmdb.org/t/p/w500/",
+=======
+    url: "https://api.themoviedb.org/3/",
+    imgUrl: "https://image.tmdb.org/t/p/w500/", 
+>>>>>>> b7e8a016562e2b2d1d25855e67d66b5e81798c94
+    key: '?api_key=1078453dc71a614c3a03d74c27fbdcb1&language=en-US',
+    articleList: [],
+    total_pages: '',
+    current_page: 1,
+    articleListLength: '',
+    limit: 10,
+    movieList: [],
+    movieIdClicked: '',
+    movieItem: {},
+    list : document.getElementById('listM'),
+    movieBackground: document.getElementById('movieBackground'),
+};
+
+//-----------------------------------------  Movies List page (GRID)
+<<<<<<< HEAD
+$(window).on('load', function (e) {
+    $('#movieBackground').hide();
+    getInfo("movie/top_rated", storage.current_page, renderList, error);     // Mivie List call
+
+});
+$(window).scroll(function() {
+    var d = document.documentElement;
+    var offset = d.scrollTop + window.innerHeight;
+    var height = d.offsetHeight;
+
+    //console.log('offset = ' + offset);
+    //console.log('height = ' + height);
+
+    if (offset === height) {
+        storage.current_page++
+        if (storage.current_page < storage.limit) {
+            getInfo("movie/top_rated", storage.current_page, renderList, error);
+            }
+    }
+});
+// ------------------------------------------ AJAX CALL FUNCTION -----------------------------------------------------------------
+function getInfo(apiName, config, render, error) {
+    $.ajax({
+        url: storage.url + apiName + storage.key  +config,
+        dataType: "json",
+        success: render,
+        error: error
+    });
+}
+
+
+// ------------------------------------------ RENDER MOVIE LIST FUNCTION -----------------------------------------------------------------
+function renderList (result, status, xhr) {
+
+    console.log(storage.articleList);
+    storage.articleList = storage.articleList.concat( result["results"]); // add new object to existing array
+    storage.total_pages =  result["total_pages"];
+    storage.articleListLength = storage.articleList.length;
+    articleList();
+
+
+=======
+$(window).on('load', function (e) {  
+        $('#movieBackground').hide();         
+            getInfo("movie/top_rated", storage.current_page, renderList, error);     // Mivie List call
+        
+});
+    // $(window).scroll(function() {
+    // if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+       // storage.current_page++
+
+            // if (storage.current_page < storage.limit) {
+                // renderList;
+            // }
+      
+    // }
+// });
+// ------------------------------------------ AJAX CALL FUNCTION -----------------------------------------------------------------
+ function getInfo(apiName, config, render, error) {
+        $.ajax({
+            url: storage.url + apiName + storage.key  +config,
+            dataType: "json",
+            success: render,
+            error: error
+        });
+    }
+ 
+
+// ------------------------------------------ RENDER MOVIE LIST FUNCTION -----------------------------------------------------------------          
+function renderList (result, status, xhr) {
+    storage.articleList = result["results"];
+    // console.log(storage.articleList);
+    // storage.articleList = storage.articleList.concat( result["results"]); // add new object to existing array
+            // storage.total_pages  =  result["total_pages"];
+            // storage.articleListLength = storage.articleList.length;
+    articleList();
+    
+    
+>>>>>>> b7e8a016562e2b2d1d25855e67d66b5e81798c94
+    $(document).ajaxStart(function () {
+        $(".imageDiv img").show();
+    });
+
+    $(document).ajaxStop(function () {
+        $(".imageDiv img").hide();
+    });
+<<<<<<< HEAD
+
+    $("input").on('change keyup paste', function (e) {
+
+        var input = document.getElementById('searchMovie');
+        getInfo("search/movie", "&page=1&include_adult=false&query=" + input.value, renderSearch, error);                  //  call search render
+
+    });
+
+
+
+
+    function articleList(){
+
+
+        var resultHtml = $("<div class=\"row\"  id=\"articleList\">");
+       // var i = (storage.current_page-1)*20;
+
+        for (i = 0; i < storage.articleList.length; i++) {
+
+            var image = storage.articleList[i]["poster_path"] == null ? "Image/no-image.png" : storage.imgUrl + storage.articleList[i]["poster_path"];
+            var cutString =  storage.articleList[i].overview.slice(0,200);
+            storage.articleList[i].overview  = cutString.slice(0, cutString.lastIndexOf('.'))+'.';
+            resultHtml.append("<div class=\"result col-12 col-sm-12 col-md-9 col-lg-3\" resourceId=\"" + storage.articleList[i]["id"] + "\">"
+                + "<div class=\"card movie-card\">"
+                +"<div class=\"rowMovieDiv row no-gutters\">"
+                + "<div class=\"imgDiv\">"
+                + "<img class=\"poster\" src=\"" + image + "\" />"
+                + "<div class=\"overlayPoster\">"
+                + "<div class=\"card-body\">"
+                + "<h4 class=\"card-title\">" + storage.articleList[i]["title"] + "</h4>"
+                + "<p class=\"card-text\">" + storage.articleList[i]["overview"] + "</p>"
+                + "<p class=\"card-footer\" resourceId=\"" + storage.articleList[i]["id"] + "\">More info</p>"
+                + "</div>"
+                + "</div>"
+                + "</div>"
+                + "</div>"
+                + "</div>"
+            )
+        }
+=======
+    
+    $("input").on('change keyup paste', function (e) {          
+
+            var input = document.getElementById('searchMovie');
+             getInfo("search/movie", "&page=1&include_adult=false&query=" + input.value, renderSearch, error);                  //  call search render
+
+});
+>>>>>>> b7e8a016562e2b2d1d25855e67d66b5e81798c94
+
+        resultHtml.append("</div>");
+        $("#listM").html(resultHtml);}
+
+}
+function ArticalItem(){
+
+}
+
+// ------------------------------------------ RENDER SEARCH LIST FUNCTION -----------------------------------------------------------------
+
+<<<<<<< HEAD
+=======
+
+
+function articleList(){
+    
+    
+        var resultHtml = $("<div class=\"row\"  id=\"articleList\">");
+        var i = (storage.current_page-1)*20;  
+
+    for (i = 0; i < storage.articleList.length; i++) {
+
+        var image = storage.articleList[i]["poster_path"] == null ? "Image/no-image.png" : storage.imgUrl + storage.articleList[i]["poster_path"];
+        var cutString =  storage.articleList[i].overview.slice(0,200);
+        storage.articleList[i].overview  = cutString.slice(0, cutString.lastIndexOf('.'))+'.';
+        resultHtml.append("<div class=\"result col-12 col-sm-12 col-md-9 col-lg-3\" resourceId=\"" + storage.articleList[i]["id"] + "\">"
+            + "<div class=\"card movie-card\">"
+                +"<div class=\"rowMovieDiv row no-gutters\">"
+                + "<div class=\"imgDiv\">"
+                        +  "<img class=\"poster\" src=\"" + image + "\" />"
+                + "</div>"
+                    + "<div class=\"overlayPoster\">"
+                        + "<div class=\"card-body\">"
+                            + "<h4 class=\"card-title\">" + storage.articleList[i]["title"] + "</h4>"
+                            + "<p class=\"card-text\">" + storage.articleList[i]["overview"] + "</p>"
+                            + "<p class=\"card-footer\" resourceId=\"" + storage.articleList[i]["id"] + "\">More info</p>"
+                        + "</div>"
+                    + "</div>"
+                + "</div>"
+            + "</div>")
+    }
+                
+    resultHtml.append("</div>");
+    $("#listM").html(resultHtml);}
+ 
+}
+function ArticalItem(){
+    
+}
+
+// ------------------------------------------ RENDER SEARCH LIST FUNCTION -----------------------------------------------------------------     
+
+>>>>>>> b7e8a016562e2b2d1d25855e67d66b5e81798c94
+function renderSearch(result, status, xhr) {
+    storage.movieList = result["results"];
+    console.log(storage);
+    var searchResult = $("<div class=\"MovieList\">");
+<<<<<<< HEAD
+
+=======
+    
+>>>>>>> b7e8a016562e2b2d1d25855e67d66b5e81798c94
+    for (var i = 0; i < result["results"].length; i++) {
+        searchResult.append("<div class=\"col-12 col-sm-12 col-md-8 input-group \" resourceId=\"" + storage.movieList[i]["id"] + "\">" + "<h4 class=\"card-title\">" + result["results"][i]["title"] + "</h4></div>")
+    }
+    searchResult.append("</div>");
+    $("#searchList").html(searchResult);
+}
+
+<<<<<<< HEAD
+// ------------------------------------------ STATUS ERROR FUNCTION -----------------------------------------------------------------
+function error(xhr, status, error) {
+    $("#listM").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+}
+=======
+// ------------------------------------------ STATUS ERROR FUNCTION -----------------------------------------------------------------   
+function error(xhr, status, error) {
+                $("#listM").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+    }
+        
+>>>>>>> b7e8a016562e2b2d1d25855e67d66b5e81798c94
---
+++ MovieDB/main1.js
@@ -0,0 +1,369 @@
+//Storage
+var storage = {
+    url: 'https://api.themoviedb.org/3/',
+    key: '?api_key=1078453dc71a614c3a03d74c27fbdcb1&language=en-US',
+    articleList: [],
+    total_pages: '',
+    current_page: 1,
+    articleListLength: '',
+    limit: 10,
+    movieList: [],
+    movieIdClicked: '',
+    movieItem: {},
+    list : document.getElementById('listM'),
+    movieBackground: document.getElementById('movieBackground'),
+};
+
+//--------------------------  Movies List page (GRID)
+
+//--------------------------  API request function
+
+function getData (apiName, num) {
+    return fetch(      storage.url + apiName + storage.key + num,
+        {  method: 'GET'  }
+    )
+}
+
+// ------------------------Build Movie list grid
+
+window.onload = function () {
+    render()
+};
+
+
+// render results of API response
+
+function render () {
+    getData('movie/top_rated', '&page=' + storage.current_page)//API request for movie list
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(res) {
+            console.log(res.results)
+            storage.articleList = storage.articleList.concat(res.results); // add new object to existing array
+            storage.total_pages	= res.total_pages;
+            storage.articleListLength = storage.articleList.length;
+            console.log(storage)
+            ArticalList()
+        })
+}
+// Create global div with id="articleList" to add all movies in grid here
+function ArticalList () {
+    //--------------------------  Infinite scroll  > render() while limit allows
+    window.onscroll = function() {
+        var d = document.documentElement;
+        var offset = d.scrollTop + window.innerHeight;
+        var height = d.offsetHeight;
+
+        //console.log('offset = ' + offset);
+        //console.log('height = ' + height);
+
+        if (offset === height) {
+
+            storage.current_page++
+
+            if (storage.current_page < storage.limit) {
+                render()
+            }
+        }
+    };
+
+    var i = (storage.current_page-1)*20+1
+    storage.articleList.slice(i).forEach(function(item) {
+        articleList.appendChild(ArticalItem(item))
+    })
+}
+
+function ArticalItem (data) {
+    // -----------------------create DOM elements of one element of grid
+
+    var itemArticle = document.createElement('div');
+
+    var itemArticleInner = document.createElement('div');
+
+    var rowMovieDiv = document.createElement('div');
+
+    var imgDiv = document.createElement('div');
+    var img = document.createElement('img');
+
+    var textDiv = document.createElement('div');
+    var cardBodyDiv = document.createElement('div');
+    var h4 = document.createElement('h4');
+    var p = document.createElement('p');
+
+
+    var moreFooterDiv = document.createElement('div');
+    var a = document.createElement('a');
+
+
+    // ------------------add classes and ids to elements
+    itemArticle.className = 'col-12 col-sm-12 col-md-9 col-lg-6';
+    itemArticleInner.className = 'card movie-card';
+    rowMovieDiv.className = 'rowMovieDiv row no-gutters';
+    imgDiv.className = 'imgDiv col-12 col-sm-4 col-md-4 col-lg-5 col-xl-4';
+    img.setAttribute('class', 'poster');
+
+
+    textDiv.className = 'textDiv col-12 col-sm-8 col-md-8 col-lg-7 col-xl-8 d-flex flex-column justify-content-around';
+
+    cardBodyDiv.className = 'card-body';
+    h4.className = 'card-title';
+    p.className = 'card-text';
+    a.id = data.id;
+
+    moreFooterDiv.className = 'card-footer';
+    a.className = 'moreInfo';
+
+    storage.movieBackground.style.display = 'none';
+
+    // -------------------define API content for the elements
+
+
+    img.src = "https://image.tmdb.org/t/p/w500/" + data.poster_path;
+
+    h4.innerText = data.title;
+
+    // function to cut visible description text to last dot
+
+    var cutString =  data.overview.slice(0,200);
+    p.innerText = cutString.slice(0, cutString.lastIndexOf('.'))+'.';
+
+    // events on More info link click
+
+    a.innerText = 'More info';
+
+    let id = storage.articleList.id
+
+    a.addEventListener('click',
+        (function(){clearList('movie')})
+    );
+    a.addEventListener('click',
+        (function(){clearList('articleList')})
+    );
+
+    a.addEventListener('click',
+
+        (function() {
+            storage.movieIdClicked = this.id
+            // console.log(storage.movieIdClicked)
+            //console.log(storage.movieItem)
+        })
+    );
+
+    a.addEventListener('click',
+        renderMovie
+    );
+
+
+    // -------------------------Append elements to page
+
+    itemArticle.appendChild(itemArticleInner)
+
+    itemArticleInner.appendChild(rowMovieDiv)
+
+    rowMovieDiv.appendChild(imgDiv)
+
+    imgDiv.appendChild(img)
+
+    rowMovieDiv.appendChild(textDiv);
+
+    textDiv.appendChild(cardBodyDiv)
+    cardBodyDiv.appendChild(h4)
+    cardBodyDiv.appendChild(p)
+
+    textDiv.appendChild(moreFooterDiv)
+    moreFooterDiv.appendChild(a)
+    storage.list.style.display = 'block';
+
+    return itemArticle
+}
+
+
+
+//--------------------------  About movie page
+
+
+function renderMovie () {
+    //----  API request for movie by ID
+    getData('movie/'+ storage.movieIdClicked, '') //--API request for deffinite movie (fetch movie id from storage.movieIdClicked parameter)
+    //----  parse response body to JSON object
+        .then(function(res) {
+            return res.json()
+
+        })
+        //----  add the result to storage object
+        .then(function(res) {
+            storage.movieItem = res
+            //console.log(storage.movieItem.title)
+            //console.log(storage.movieItem)
+            //console.log(storage.articleList)
+            MovieItem()
+        })
+
+}
+//--------------------------  create dom elements for about movie page
+
+//--------------------------  create BACK link for user to go back to movies list
+function MovieItem () {
+    let backLinkDiv = document.createElement('div');
+    backLinkDiv.className = 'col-12 col-sm-12 col-md-12 col-lg-12 my-3';
+    let backLink = document.createElement('button');
+    backLink.addEventListener('click',
+        (function(){clearList('movie')})
+    );
+    backLink.addEventListener('click',
+        ArticalList                            // надо передавать тело
+    );
+
+
+    backLink.className = 'btn btn-outline-info my-2 my-sm-0';
+    backLink.innerText = 'Back to Movie List';
+
+
+    movie.appendChild(backLinkDiv);
+    backLinkDiv.appendChild(backLink);
+
+
+    // -----------------------создаем переменную DOM-элементы
+    let itemMovie = document.createElement('h4');
+    let back_poster = document.createElement('img');
+    let imgAbout = document.createElement('div');
+    // console.log(imgAbout);
+    let imgAboutSrc = document.createElement('img');
+    let textAbout = document.createElement('div');
+    let pTextAbout = document.createElement('p');
+    // ------------------добавили HTML class для дальнейшей стилизации
+    itemMovie.className = 'card-title';
+    imgAbout.className = 'col-12 col-sm-4 col-md-4 col-lg-5 col-xl-4';
+    textAbout.className = 'col-12 col-sm-8 col-md-8 col-lg-7 col-xl-8 d-flex flex-column ';
+    imgAboutSrc.className = 'imgAboutSrc'
+    // -------------------определяем наполнение этих элементов из API
+
+    itemMovie.innerText = storage.movieItem.title;
+    imgAboutSrc.src = 'https://image.tmdb.org/t/p/w500' + storage.movieItem.poster_path;
+    pTextAbout.innerText = storage.movieItem.overview;
+    //   back_poster.src = 'https://image.tmdb.org/t/p/w500' + storage.movieItem.backdrop_path;
+    // -------------------------Выводим элементы на страницу
+
+    //imgAbout.appendChild(imgAboutSrc);
+
+    movie.appendChild(imgAbout);
+    imgAbout.appendChild(imgAboutSrc);
+    movie.appendChild(textAbout);
+    textAbout.appendChild(itemMovie);
+    textAbout.appendChild(pTextAbout);
+
+    storage.movieBackground.style.display = 'block';
+    storage.movieBackground.style.background = `url('https://image.tmdb.org/t/p/w500${storage.movieItem.backdrop_path}') no-repeat`;
+    storage.movieBackground.style.backgroundSize = "cover";
+    // movieBackground.style.backgroundColor
+    //--------------------------  Infinite scroll  > render() while limit allows
+    window.onscroll = function() {
+        return null;
+    };
+    storage.list.style.display = 'none';
+}
+//--------------------------  clearList function to use on different events
+function clearList(elId)
+{
+    document.getElementById(elId).innerHTML = "";
+}
+
+// ----------------------------------Search movies section ---------------------------------------------------------
+// -------------------------- API request to show search results
+
+
+// -------------------------- Sarch Movies
+let input = document.getElementById('searchMovie');
+
+input.oninput = function() {
+    clearSearch('searchList')
+    getData('search/movie', '&page=1&include_adult=false&query=' + input.value)
+        .then(function(res) { return res.json()})
+        .then(function(res) {
+            //console.log(res)
+            storage.movieList = res.results
+            searchList.appendChild(MovieList())
+        })
+};
+
+
+function clearSearch(el){
+    document.getElementById(el).innerHTML = "";
+};
+function clearSearchValue(el){
+    document.getElementById(el).value = "";
+};
+
+function MovieList () {
+    var div = document.createElement('div')
+    div.className = 'MovieList'
+    if (input.value) {
+        storage.movieList.forEach(function (item) {
+            div.appendChild(ItemMovieFromList(item));
+        })
+    }
+    return div
+};
+
+
+function ItemMovieFromList(data) {
+    var searchList = document.createElement('div');
+
+    var searchListUl = document.createElement('ul');
+    var searchListLi = document.createElement('li');
+
+
+    var aSearch = document.createElement('a')
+
+    aSearch.className = 'ItemMovieFromSearchList'
+    aSearch.innerText = data.title
+    let idFoundMovie = data.id
+    aSearch.id = idFoundMovie;
+    // ----------- clear movie description div (if any)
+    aSearch.addEventListener('click',
+        (function(){clearSearchValue('searchMovie')})
+    );
+    aSearch.addEventListener('click',
+        (function(){clearSearch('searchList')})
+    );
+    aSearch.addEventListener('click',
+        (function(){clearList('movie')})
+    );
+
+    aSearch.addEventListener('click',
+        (function(){clearList('articleList')})
+    );
+
+    aSearch.addEventListener('click',
+
+        (function() {
+            storage.movieIdClicked = this.id
+            // console.log(storage.movieIdClicked)
+            //console.log(storage.movieItem)
+        })
+
+    );
+
+    aSearch.addEventListener('click',
+
+        renderMovie
+
+    );
+
+
+
+
+    searchList.className = 'col-12 col-sm-12 col-md-8 input-group ';
+
+    searchListUl.className = 'suggests-component';
+
+    searchList.appendChild(searchListUl)
+    searchListUl.appendChild(aSearch)
+    aSearch.appendChild(searchListLi)
+
+
+
+    return searchList
+}
+// }
